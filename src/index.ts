61f287e132cb09e3fcca25c94ae3a93a02960c3c
Jatin <jatin.parate@mlveda.com>
2022-06-05 13:11:13 +0530

    Setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import dotEnv from 'dotenv';
+import app from './app';
 
 dotEnv.config({
   path: `.env.${process.env.NODE_ENV}`,
+});
+
+let port: number = parseInt(process.env.PORT as string, 10);
+
+if (!port || Number.isNaN(port)) {
+  port = 3000;
+}
+
+app.listen(port, () => {
+  console.info(`Server is running on port ${port}`);
 });
